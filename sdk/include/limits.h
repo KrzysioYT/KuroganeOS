22760198efce16a050c9846dc3cb3d9d1f7c96ed
KrzysioYT <59750414+KrzysioYT@users.noreply.github.com>
2026-08-18 11:03:21 +0200

    Add freestanding limits header

---
+++ sdk/include/limits.h
@@ -0,0 +1,46 @@
+#ifndef KUROGANE_LIBC_LIMITS_H
+#define KUROGANE_LIBC_LIMITS_H
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC system_header
+#endif
+
+#if defined(__STDC_HOSTED__) && __STDC_HOSTED__ == 1
+#include_next <limits.h>
+#else
+
+/* Freestanding x86-64 limits derived from compiler-provided ABI constants. */
+#define CHAR_BIT __CHAR_BIT__
+
+#define SCHAR_MAX __SCHAR_MAX__
+#define SCHAR_MIN (-SCHAR_MAX - 1)
+#define UCHAR_MAX (SCHAR_MAX * 2U + 1U)
+
+#if defined(__CHAR_UNSIGNED__)
+#define CHAR_MIN 0
+#define CHAR_MAX UCHAR_MAX
+#else
+#define CHAR_MIN SCHAR_MIN
+#define CHAR_MAX SCHAR_MAX
+#endif
+
+#define SHRT_MAX __SHRT_MAX__
+#define SHRT_MIN (-SHRT_MAX - 1)
+#define USHRT_MAX (SHRT_MAX * 2U + 1U)
+
+#define INT_MAX __INT_MAX__
+#define INT_MIN (-INT_MAX - 1)
+#define UINT_MAX (INT_MAX * 2U + 1U)
+
+#define LONG_MAX __LONG_MAX__
+#define LONG_MIN (-LONG_MAX - 1L)
+#define ULONG_MAX (LONG_MAX * 2UL + 1UL)
+
+#define LLONG_MAX __LONG_LONG_MAX__
+#define LLONG_MIN (-LLONG_MAX - 1LL)
+#define ULLONG_MAX (LLONG_MAX * 2ULL + 1ULL)
+
+/* KuroganeOS currently exposes single-byte execution characters only. */
+#define MB_LEN_MAX 1
+
+#endif
+#endif
